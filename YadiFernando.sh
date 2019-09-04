fda53158d6d353ee8b218ab70d18c753aa716d4e
YadiFernando <51882097+YadiFernando@users.noreply.github.com>
2019-09-05 00:44:57 +0700

    Update YadiFernando.sh

--- YadiFernando.sh
+++ YadiFernando.sh
@@ -7,24 +7,31 @@ red='\033[31;1m'
 white='\033[37;1m'                                           
 yellow='\033[33;1m'
 sleep 1
-echo "\033[32;1mCreated By Yadi Fernando"
-sleep 1
+echo $yellow "<===================================>"
+echo $green "Created By : Yadi Fernando"
+echo $red "Channel YT : Official Yadi Fernando"
+echo $yellow "<===================================>"
+sleep 2
 echo
 echo "\033[34;1m"
 figlet "HALLO"
+echo $white "WELCOME TO MY SCRIPT"
+sleep 1
+echo $green "SCRIPT BY YADI FERNANDO"
+sleep 2
 echo
-echo $yellow "Kamu bisa hubungi saya di : "
+echo $yellow "You Can Contact Me From: "
 sleep 2
 echo $green
 figlet "W A"
 echo "085706249638"
 echo
 sleep 2
-echo $red
-figlet "Y T"
-echo "Official Yadi Fernando"
-echo
-sleep 2
+
+
+
+
+
 echo $blue
 figlet "F B"
 echo "Yadi Fernando"
@@ -35,33 +42,39 @@ figlet "I G"
 echo "@yadi_fernando123"
 sleep 1
 echo ""
-echo "\033[31;1mSilahkan Pilih Guys"
-echo
-echo $blue "1.> Stabilkan Jaringan"
-echo
-echo $green "2.> Stabilkan Jaringan v2"
-echo
-echo $red "3.> Stabilkan Jaringan Saat Nonton Youtube"
-echo
-echo $yellow "4.> Stabilkan Jaringan Saat Browsing"
-echo
-echo $purple "5.> Keluar Aja"
-echo
+echo "\033[31;1mSilahkan Pilih Guys :"
+echo $green "<===================================>"
+echo $blue "1.} Stabilkan Jaringan"
+echo $purple "<===================================>"
+echo $green "2.} Stabilkan Jaringan v2"
+echo $yellow "<===================================>"
+echo $red "3.} Stabilkan Jaringan Saat Youtube-an"
+echo $white "<===================================>"
+echo $yellow "4.} Stabilkan Jaringan Saat Browsing"
+echo $blue "<===================================>"
+echo $red "5.} COMING SOON..."
+echo $green "<===================================>"
+echo white "6.} KELUAR AJA...!"
+echo $purple "<===================================>"
 echo "\033[32;1m"
-read -p "#Root@YadiFernando : " bro
+read -p "#Root@OYF : " bro
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
 figlet "Yadi"
+echo $blue "Stabilkan Jaringan v1 By Yadi"
+sleep 2
 echo $blue
-ping 8.8.8.8
+ping -s1000 8.8.8.8
 fi
 
 if [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
 figlet "By Yadi"
+echo $green "Stabilkan Jaringan v2 By Yadi"
+sleep 2
 echo $green
 ping -s1000 1.1.1.1
 fi
@@ -70,6 +83,10 @@ if [ $bro = 3 ] || [ $bro = 3 ]
 then
 clear
 figlet "Youtube"
+echo $yellow "Jangan Lupa" $red"SUBSCRIBE" $yellow"Channel Youtube saya :"
+sleep 1
+echo $green "Official Yadi Fernando"
+sleep 2
 echo $red
 ping -s1000 74.125.24.138
 fi
@@ -78,6 +95,8 @@ if [ $bro = 4 ] || [ $bro = 4 ]
 then
 clear
 figlet "Browsing"
+echo $yellow "Insya Allah Browsing Jadi Lancar Jaya"
+sleep 2
 echo $yellow
 ping -s1000 74.125.68.103
 fi
@@ -85,8 +104,29 @@ fi
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
 clear
+echo $white
+figlet "COMING"
+figlet "SOON"
+echo $yellow "Terimakasih Sudah Mencoba"
+echo "Script Buatan Yadi Fernando"
+sleep 2
+echo $white "Tunggu Update Script Selanjutnya"
+figlet "Bye"
+sleep 2
+exit
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+
+then
+clear
 figlet "Thanks"
 echo $green "SAMPAI JUMPA DI LAIN WAKTU"
+sleep 2
+echo $yellow "SEMOGA HARIMU MENYENANGKAN"
+sleep 1
+echo $blue "GUNAKAN SCRIPT INI DENGAN BIJAK"
+sleep 3
 echo $white
-cd ~
+exit
 fi
